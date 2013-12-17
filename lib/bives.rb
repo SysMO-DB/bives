8d9f48fd7dfcda3186b527aab0fe2db75d3eeea3
Stuart Owen <stuart.owen@manchester.ac.uk>
2013-12-17 12:27:05 +0000

    check the jar when included and before trying to compare

--- lib/bives.rb
+++ lib/bives.rb
@@ -6,16 +6,27 @@ module Bives
 
   end
 
+  class InitialisationException < Exception
+
+  end
+
   JAR_FILENAME = "BiVeS-1.1.2-SNAPSHOT-fat.jar"
   JAR_FILEPATH = File.join File.dirname(__FILE__),"jars","#{JAR_FILENAME}"
 
   def self.included(mod)
     unless File.exist?(JAR_FILEPATH)
-      raise "Jar file #{JAR_FILEPATH} not found"
+      raise InitialisationException.new("Jar file #{JAR_FILEPATH} not found")
+    end
+  end
+
+  def check_jar
+    unless File.exist?(JAR_FILEPATH)
+      raise InitialisationException.new("Jar file #{JAR_FILEPATH} not found")
     end
   end
 
   def compare file1,file2,opts=["reportHtml"]
+    check_jar
     cmd_opts = opts.collect{|o| "--#{o}"}.join(" ")
     command = "java -jar #{JAR_FILEPATH} #{cmd_opts} #{file1} #{file2}"
 
